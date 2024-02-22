a28c7c4bab1fe62621307cc6913ee74cfdf3cd95
AEGIR24 <160857424+AEGIR24@users.noreply.github.com>
2024-02-22 08:12:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
+import streamlit as st
+import os
+import time
+import glob
+import os
+from gtts import gTTS
+from PIL import Image
 
+st.title(Interfaces Multimodales.)
+image = Image.open('mago.jpg')
+
+st.image(image, width=200)
